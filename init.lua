e0f3d0d50e5a08957e2ebeb916327149a09ab9b3
chubi-x <chubixax@protonmail.com>
2024-10-21 11:29:20 +0100

    fix lsp lines and move keymaps and other options into correct config files

--- init.lua
+++ init.lua
@@ -1,13 +1,5 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.keymap.set("n", "<C-Down>", "10j", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Up>", "10k", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Left>", "10h", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Right>", "10l", { noremap = true, silent = true })
-vim.keymap.set("n", "<C-Shift-Down>", "yyp", { noremap = true, silent = true })
-vim.opt.number = true
-vim.opt.relativenumber = false
-vim.diagnostic.config({ virtual_text = false })
 require("neo-tree").setup({
   filesystem = {
     filtered_items = {
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,8 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.set("n", "<C-Down>", "10j", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Up>", "10k", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Left>", "10h", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-Right>", "10l", { noremap = true, silent = true })
+vim.keymap.set("n", "<C-S-Down>", "yyp", { noremap = true, silent = true })
