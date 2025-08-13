71a80d085ab8dceb042d442aa8c5b690c4e03e8e
chubi-x <chubixax@protonmail.com>
2025-08-13 22:25:38 +0100

    add some ai plugins

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -9,3 +9,14 @@ vim.keymap.set("", "<Leader>xd", require("lsp_lines").toggle, { desc = "Toggle l
 vim.keymap.set("", "<Leader>gb", "<cmd>BlameToggle window<CR>", { desc = "Toggle git blame" })
 
 vim.keymap.set("", "<Leader>rd", "<cmd>PythonCopyReferenceDotted <CR>", { desc = "Copy Python Reference" })
+vim.keymap.set(
+  { "n", "v" },
+  "<Leader>ac",
+  "<cmd>CodeCompanionActions<cr>",
+  { noremap = true, silent = true, desc = "Open CodeCompanionActions" }
+)
+-- vim.keymap.set({ "n", "v" }, "<Leader>a", "<cmd>CodeCompanionChat Toggle<cr>", { noremap = true, silent = true })
+-- vim.keymap.set("v", "ga", "<cmd>CodeCompanionChat Add<cr>", { noremap = true, silent = true })
+
+-- Expand 'cc' into 'CodeCompanion' in the command line
+vim.cmd([[cab cc CodeCompanion]])
